5abcd82af978f9b4012cf862c903f3929adebec9
Nahida <nahidazakir15@gmail.com>
2022-11-24 11:29:32 +0600

    database connected

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
@@ -11,9 +11,15 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.twtll.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
 
 async function run() {
     try {
+        const productsCollection = client.db('Style-Hub').collection('products');
 
     }
     finally {
